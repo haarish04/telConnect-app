b9a1933f1d675b7d5b6608dca6bba65a60a3ccd7
sukruthiashok-02 <sukruthi.ashok@team.telstra.com>
2024-09-08 10:42:55 +0530

    No idea..

--- src/pages/SupportPage.jsx
+++ src/pages/SupportPage.jsx
@@ -1,7 +1,7 @@
 import NavBar from "../components/NavBar";
 import Support from "../components/Support";
 
-export default function RegisterPage() {
+export default function SupportPage() {
   return (
     <div>
       <NavBar />
